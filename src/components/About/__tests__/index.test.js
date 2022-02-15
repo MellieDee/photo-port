361b8f41b58d61937ef101d60a31d1ca6c94c526
Mellie Dee <89175650+MellieDee@users.noreply.github.com>
2022-02-14 16:43:03 -0800

    created About test suite

---
+++ src/components/About/__tests__/index.test.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { render, cleanup } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+
+import About from "..";
+
+const { asFragment } = render(<About />);
+
+//configure testing environment by adding a utility function to keep things clean
+afterEach(cleanup);
+
+// declare component to be tested
+describe('About component', () => {
+  //1st test - baseline for rendering:
+  //it function:  first argument - string declares what is being tested
+  // second argument - callback function runs the test. (could also use test)
+  it('renders', () => {
+    render(<About />);
+  });
+
+  //2nd test - Test Case:
+  it('matches snapshot DOM node structure', () => {
+    // render About
+
+    expect(asFragment()).toMatchSnapshot();
+  });
+})
